61b1afae9c0c33420ddb41c1238376993d27a34e
JulienSic <julien.sicard@epitech.eu>
2026-02-17 10:01:46 +0100

    Feat : fully working model, with Shape Keys, and custom materials

--- TJS_ComplexInteractivePortfolio/Complex3DPortfolio/src/Components/TestBreakWall.tsx
+++ TJS_ComplexInteractivePortfolio/Complex3DPortfolio/src/Components/TestBreakWall.tsx
@@ -1,41 +1,145 @@
 import { useLoader } from '@react-three/fiber'
-import React, {useMemo} from 'react'
+import React, {useEffect, useMemo, useRef} from 'react'
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
-import {Mesh} from "three";
+import {DoubleSide, Mesh, MeshStandardMaterial} from "three";
+import {Pane} from "tweakpane";
+import gsap from 'gsap';
 
 export default function TestBreakWall() {
   const customModel = useLoader(GLTFLoader, '/assets/models/TJS_TestBreakWall.glb');
-  const modelScale = 0.01;
+  const modelScale = 0.3;
   const breakWall = React.useRef();
+
+  const morphMeshesRef = useRef<Mesh[]>([]);
 
   // Extract the geometry from the model
   const customMesh = useMemo(() => {
 
-    let foundMesh: Mesh | null = null;
-
     const sceneClone = customModel.scene.clone(true);
 
+    morphMeshesRef.current = [];
+
     sceneClone.traverse((child) => {
-      if (child instanceof Mesh && !foundMesh) {
-        foundMesh = child;
-        if (Array.isArray(foundMesh.material)) {
-          foundMesh.material.forEach((material) => {
-            material.side = 2;
+      if (child.isMesh) {
+
+        const applyColorToSubMeshes = (material) => {
+
+          const matName = material.name.toLowerCase();
+          material.side = DoubleSide;
+
+          if (matName.includes('wall')) {
+            material.color.set('#392FEB');
+          }
+          else if (matName.includes('inside')) {
+            material.color.set('#EBDE07');
+          }
+          else {
+            material.color.set('white');
+            console.log("Unknown Material, setting default value");
+          }
+        };
+
+        child.castShadow = true;
+        child.receiveShadow = true;
+
+        if (Array.isArray(child.material)) {
+          child.material.forEach((mat) => {
+
+            if (child.material[0] instanceof MeshStandardMaterial) {
+              child.material[0].side = DoubleSide;
+              child.material[0].color.set('#EBDE07');
+            }
+
+            // Slot 1
+            if (child.material[1] instanceof MeshStandardMaterial) {
+              child.material[1].side = DoubleSide;
+              child.material[1].color.set('#392FEB');
+            }
+
           });
-        } else {
-          foundMesh.material.side = 2;
+        } else if (child.material instanceof MeshStandardMaterial) {
+          child.material.side = DoubleSide;
+          applyColorToSubMeshes(child.material);
+
+          //child.material.color.set(57,47,235);
+          //child.material.color.set(240,229,38);
         }
 
-        if (foundMesh.geometry) {
-          foundMesh.geometry.computeBoundingSphere();
-          foundMesh.geometry.computeVertexNormals();
+        // Checks if there is a Shapekey in the mesh
+        if (child.morphTargetInfluences) {
+          console.log(`Found Shape Keys for ${child.name}`, child.morphTargetDictionnary);
+          // Store the reference of the mesh
+          morphMeshesRef.current.push(child);
         }
       }
+      if (child.geometry) {
+        child.geometry.computeBoundingSphere();
+        child.geometry.computeVertexNormals();
+      }
     });
-    return foundMesh;
+    return sceneClone;
   }, [customModel]);
 
+  console.log(customMesh);
   if (!customMesh) return null;
+
+  useEffect(() => {
+    const pane = new Pane({ title: 'Test Break Wall' });
+
+    const params = {
+      explosion: 0,
+    }
+
+    pane.addBinding(params, 'explosion',   {min: 0, max: 1, step: 0.01})
+        .on('change', (ev) => {
+          morphMeshesRef.current.forEach((mesh) => {
+            if (mesh.morphTargetInfluences) {
+              mesh.morphTargetInfluences[0] = ev.value;
+            }
+          });
+        })
+
+    const btn = pane.addButton ({ title: 'Wall Explosion' });
+
+    btn.on('click', () => {
+      gsap.killTweensOf(params);
+      gsap.to(params, {
+        explosion: 1,
+        duration: 1.5,
+        ease: "power2.out",
+        onUpdate: () => {
+          pane.refresh();
+
+          morphMeshesRef.current.forEach((mesh) => {
+            if (mesh.morphTargetInfluences) {
+              mesh.morphTargetInfluences[0] = params.explosion;
+            }
+          });
+        }
+      })
+    } )
+
+    const btnR = pane.addButton ({ title: 'Reset Explosion'});
+
+    btnR.on('click', () => {
+      gsap.killTweensOf(params);
+      gsap.to(params, {
+        explosion:0,
+        duration: 0.5,
+        ease: "power2.out",
+        onUpdate: () => {
+          pane.refresh();
+
+          morphMeshesRef.current.forEach((mesh) => {
+            if (mesh.morphTargetInfluences) {
+              mesh.morphTargetInfluences[0] = params.explosion;
+            }
+          });
+        }
+      })
+
+    })
+  })
 
   return (
       <primitive
